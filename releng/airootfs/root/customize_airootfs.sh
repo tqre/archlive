981cd2c93e54e8bbe4065300599d448332d63391
tqre <tkuure@gmail.com>
2019-04-15 23:29:01 +0300

    Added dhcpcd and salt-minion to startup daemons

--- releng/airootfs/root/customize_airootfs.sh
+++ releng/airootfs/root/customize_airootfs.sh
@@ -22,3 +22,6 @@ sed -i 's/#\(HandleLidSwitch=\)suspend/\1ignore/' /etc/systemd/logind.conf
 systemctl enable pacman-init.service choose-mirror.service
 systemctl set-default multi-user.target
 
+systemctl start dhcpcd
+
+#systemctl start salt-minion
