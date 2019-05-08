284d82b870e41ad872bb604458f4fb437e3dad38
tqre <tkuure@gmail.com>
2019-05-08 21:11:41 +0300

    removed dhcpcd service as it conflicts sometimes with pre-existing dhcp configuration

--- releng/airootfs/root/customize_airootfs.sh
+++ releng/airootfs/root/customize_airootfs.sh
@@ -21,8 +21,7 @@ sed -i 's/#\(HandleLidSwitch=\)suspend/\1ignore/' /etc/systemd/logind.conf
 
 systemctl enable pacman-init.service choose-mirror.service
 systemctl set-default multi-user.target
-systemctl enable dhcpcd.service
 
 sed -i '149s/encoding=encoding/raw=False/' /usr/lib/python2.7/site-packages/salt/payload.py
 
-systemctl enable prepare-salt-minion.service
+systemctl enable prepare-salt-master.service
