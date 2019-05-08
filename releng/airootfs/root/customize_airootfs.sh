477ca223539eaa447884118bd47f19217d57b0de
tqre <tkuure@gmail.com>
2019-05-08 17:49:05 +0300

    enabled prepare-salt-master.service

--- releng/airootfs/root/customize_airootfs.sh
+++ releng/airootfs/root/customize_airootfs.sh
@@ -23,5 +23,6 @@ systemctl enable pacman-init.service choose-mirror.service
 systemctl set-default multi-user.target
 
 systemctl enable dhcpcd.service
+systemctl enable prepare-salt-master.service
 
 sed -i '149s/encoding=encoding/raw=False/' /usr/lib/python2.7/site-packages/salt/payload.py
